869d7b5bfac9be7432752633b7be674f33bb8d8b
douNine <bug@nuaa.edu.cn>
2019-04-06 21:48:26 +0800

    tab error

--- entry.py
+++ entry.py
@@ -21,20 +21,20 @@ def main():
         print('一个新请求到来，来自：%s' % str(client_addr))
 
         recv_data = tcp_client_socket.recv(1024).decode('utf-8')   # 从客户端接受消息，最多1024字节。recv_data为字节类型，.decode()将recv_data转化成字符型
-				print('请求发送的命令是%s' % recv_data)
+        print('请求发送的命令是%s' % recv_data)
 
-				retMsg = ''
-				if recv_data == 'start':
-				    print('send start to gdb')
+        retMsg = ''
+        if recv_data == 'start':
+            print('send start to gdb')
             retMsg = gdbSer.start()
-				elif recv_data == 'continue':
-				    print('send continue to gdb')
+        elif recv_data == 'continue':
+            print('send continue to gdb')
             retMsg = gdbSer.contin()
-				else:
-				    print('no this commond')
-            print(retMsg)
+        else:
+            print('no this commond')
+        print(retMsg)
 
-				tcp_client_socket.send(retMsg.encode('utf-8'))	# 将字符串进行字节编码
+        tcp_client_socket.send(retMsg.encode('utf-8'))	# 将字符串进行字节编码
 
         # 关闭与客户端的连接
         tcp_client_socket.close()
